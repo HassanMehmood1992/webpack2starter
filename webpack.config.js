52935b40d68909db16b30483f6525a1b0ddfe92f
Hassan Mehmood <hmehmood92@gmail.com>
2018-05-06 15:38:01 +0500

    html plugin added

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+module.exports = {
+    entry:"./src/app.js",
+    output:{
+        path: path.resolve(__dirname, './dist'),
+        filename:'app.bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+            title: 'My App',
+            minify: {
+                collapseWhitespace: true,
+            },
+            hash: true,
+            template: './src/index.ejs'
+        }   
+    )]
+}
